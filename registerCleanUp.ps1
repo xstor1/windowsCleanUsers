9f2e53687a1d130d2c35adb1387827f5e0fcd041
Vojtěch Štor <vojtastor32@gmail.com>
2023-08-13 15:38:57 +0200

    Create registerCleanUp.ps1

---
+++ registerCleanUp.ps1
@@ -0,0 +1,28 @@
+# Cesta k souboru, který obsahuje čisticí skript
+$scriptPath = "C:\scripts\CleanProfiles.ps1"
+
+# Obsah čisticího skriptu
+$scriptContent = @"
+Get-WmiObject Win32_UserProfile | Where-Object {
+    `$_.Special -eq `$false -and `$_.LocalPath -notmatch 'C:\\Users\\admin' -and `$_.LocalPath -notmatch 'C:\\Users\\admin1'
+} | ForEach-Object {
+    Remove-WmiObject `$_
+}
+"@
+
+# Vytvoření čisticího skriptu
+Set-Content -Path $scriptPath -Value $scriptContent
+
+# Vytvoření akce, která spustí váš skript
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File $scriptPath"
+
+# Nastavení spouštění úkolu jednou za měsíc
+$trigger = New-ScheduledTaskTrigger -At '1AM' -RepetitionInterval (New-TimeSpan -Days 30)
+
+# Nastavení, která umožní spuštění úkolu po startu, pokud byl přeskočen
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DontStopOnIdleEnd -StartWhenAvailable
+
+# Registrace úkolu s těmito nastaveními
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Clean User Profiles" -Description "Monthly task to clean user profiles" -User "NT AUTHORITY\SYSTEM" -RunLevel Highest -Settings $settings
+
+Write-Host "Task registered successfully."
